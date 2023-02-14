0ef793b54ca067fd31811e13db124121c6cf0186
Neil Williams <neil.williams@dvla.gov.uk>
2023-02-14 18:11:56 +0000

    Adding support for redis 5

--- lib/instana/instrumentation/redis.rb
+++ lib/instana/instrumentation/redis.rb
@@ -3,6 +3,8 @@
 
 module Instana
   module RedisInstrumentation
+
+    # support redis-rb < 5
     def call(*args, &block)
       kv_payload = { redis: {} }
       dnt_spans = [:redis, :'resque-client', :'sidekiq-client']
@@ -60,6 +62,36 @@ module Instana
       ensure
         ::Instana.tracer.log_exit(:redis, kv_payload)
       end
+    end
+  end
+
+  # support redis-rb >= 5
+  def call_v(*args, &block)
+    kv_payload = { redis: {} }
+    dnt_spans = [:redis, :'resque-client', :'sidekiq-client']
+
+    if !Instana.tracer.tracing? || dnt_spans.include?(::Instana.tracer.current_span.name) || !Instana.config[:redis][:enabled]
+      return super(*args, &block)
+    end
+
+    begin
+      ::Instana.tracer.log_entry(:redis)
+
+      begin
+        kv_payload[:redis][:connection] = "#{self.host}:#{self.port}"
+        kv_payload[:redis][:db] = db.to_s
+        kv_payload[:redis][:command] = args[0][0].to_s.upcase
+      rescue
+        nil
+      end
+
+      super(*args, &block)
+    rescue => e
+      ::Instana.tracer.log_info({ redis: {error: true} })
+      ::Instana.tracer.log_error(e)
+      raise
+    ensure
+      ::Instana.tracer.log_exit(:redis, kv_payload)
     end
   end
 end
