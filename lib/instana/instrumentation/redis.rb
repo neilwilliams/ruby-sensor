e9e67f45a15d2a89a4a29d617618a5b5cbd7d65e
Neil Williams <neil.williams@dvla.gov.uk>
2023-02-14 20:06:42 +0000

    Removing pre v5 redis overides, they don't work together with the new v5 changes

--- lib/instana/instrumentation/redis.rb
+++ lib/instana/instrumentation/redis.rb
@@ -3,9 +3,7 @@
 
 module Instana
   module RedisInstrumentation
-
-    # support redis-rb < 5
-    def call(*args, &block)
+    def call_v(*args, &block)
       kv_payload = { redis: {} }
       dnt_spans = [:redis, :'resque-client', :'sidekiq-client']
 
@@ -32,66 +30,6 @@ module Instana
       ensure
         ::Instana.tracer.log_exit(:redis, kv_payload)
       end
-    end
-
-    def call_pipeline(*args, &block)
-      kv_payload = { redis: {} }
-      dnt_spans = [:redis, :'resque-client', :'sidekiq-client']
-
-      if !Instana.tracer.tracing? || dnt_spans.include?(::Instana.tracer.current_span.name) || !Instana.config[:redis][:enabled]
-        return super(*args, &block)
-      end
-
-      begin
-        ::Instana.tracer.log_entry(:redis)
-
-        pipeline = args.first
-        begin
-          kv_payload[:redis][:connection] = "#{self.host}:#{self.port}"
-          kv_payload[:redis][:db] = db.to_s
-          kv_payload[:redis][:command] = pipeline.is_a?(::Redis::Pipeline::Multi) ? 'MULTI' : 'PIPELINE'
-        rescue
-          nil
-        end
-
-        super(*args, &block)
-      rescue => e
-        ::Instana.tracer.log_info({ redis: {error: true} })
-        ::Instana.tracer.log_error(e)
-        raise
-      ensure
-        ::Instana.tracer.log_exit(:redis, kv_payload)
-      end
-    end
-  end
-
-  # support redis-rb >= 5
-  def call_v(*args, &block)
-    kv_payload = { redis: {} }
-    dnt_spans = [:redis, :'resque-client', :'sidekiq-client']
-
-    if !Instana.tracer.tracing? || dnt_spans.include?(::Instana.tracer.current_span.name) || !Instana.config[:redis][:enabled]
-      return super(*args, &block)
-    end
-
-    begin
-      ::Instana.tracer.log_entry(:redis)
-
-      begin
-        kv_payload[:redis][:connection] = "#{self.host}:#{self.port}"
-        kv_payload[:redis][:db] = db.to_s
-        kv_payload[:redis][:command] = args[0][0].to_s.upcase
-      rescue
-        nil
-      end
-
-      super(*args, &block)
-    rescue => e
-      ::Instana.tracer.log_info({ redis: {error: true} })
-      ::Instana.tracer.log_error(e)
-      raise
-    ensure
-      ::Instana.tracer.log_exit(:redis, kv_payload)
     end
   end
 end
